58c3d9c2f1bf66c28252b825823af242205db0ad
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-01-22 11:23:54 +0100

    Add check guesses method

--- Mastermind.rb
+++ Mastermind.rb
@@ -2,13 +2,14 @@ module  Mastermind
     class  << self
         private 
         @@colors_array = ['r', 'g', 'b', 'y', 'o', 'p']
-        @@correct_choice = 'bl'
-        @@wrong_position = 'w'
-        @@nothing = 'gr'
+        @@correct_choice = 1
+        @@wrong_position = 2
+        @@nothing = 3
         @@choice_arr = []
         @@combination_arr = []
         @@position_check_arr = []
         def newCombination
+            @@combination_arr = []
             4.times {@@combination_arr.push(@@colors_array[rand(0..@@colors_array.length - 1)])}
         end
         def choiceInput
@@ -24,12 +25,29 @@ module  Mastermind
             end
             @@choice_arr.push(tempC)
         end
+        def pushRightColor(col, index)
+            if @@choice_arr[index] == col
+                @@position_check_arr.push(@@correct_choice)
+            else
+                @@position_check_arr.push(@@wrong_position)
+            end
+        end
+        def compareInputCombination
+            @@combination_arr.each_with_index do 
+                |comb_col, comb_index|
+                does_it_exist = @@choice_arr.any?{|col| col == comb_col}
+                does_it_exist ? pushRightColor(comb_col, comb_index): @@position_check_arr.push(@@nothing)
+            end
+            @@position_check_arr = @@position_check_arr.sort
+        end
         public 
         def testMethod
             newCombination
-           4.times{choiceInput}
-           puts "Combination array #{@@combination_arr}"
-           puts "Choice array #{@@choice_arr}"
+            4.times{choiceInput}
+            compareInputCombination
+            p @@combination_arr
+            p @@choice_arr
+            print @@position_check_arr
         end
     end
 end
