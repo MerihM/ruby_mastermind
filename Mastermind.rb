0f7b70e6fee02df9e15cd6ac3d15c62c661a0bf6
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-01-22 09:58:03 +0100

    Add player input method

--- Mastermind.rb
+++ Mastermind.rb
@@ -9,12 +9,27 @@ module  Mastermind
         @@combination_arr = []
         @@position_check_arr = []
         def newCombination
-            @@combination_arr.push(rand(0..@@colors_array.length - 1))
+            4.times {@@combination_arr.push(@@colors_array[rand(0..@@colors_array.length - 1)])}
+        end
+        def choiceInput
+            puts `clear`
+            p "Select color"
+            tempC = gets.chomp
+            isItAvailable = @@colors_array.any?{|color| color == tempC.downcase}
+            until isItAvailable
+                puts `clear`
+                p "Wrong choice, select again"
+                tempC = gets.chomp
+                isItAvailable = @@colors_array.any?{|color| color == tempC.downcase}
+            end
+            @@choice_arr.push(tempC)
         end
         public 
         def testMethod
-            4.times{newCombination}
-            p @@combination_arr
+            newCombination
+           4.times{choiceInput}
+           puts "Combination array #{@@combination_arr}"
+           puts "Choice array #{@@choice_arr}"
         end
     end
 end
