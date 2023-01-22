3283534f2f6207ffa8c4cfc8f96691cfc7679fcf
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-01-22 14:02:08 +0100

    Add check win method

--- Mastermind.rb
+++ Mastermind.rb
@@ -8,12 +8,18 @@ module  Mastermind
         @@choice_arr = []
         @@combination_arr = []
         @@position_check_arr = []
+        @@win = false
+        @@round = 0
+        @@round_arr = []
         def newCombination
             @@combination_arr = []
             4.times {@@combination_arr.push(@@colors_array[rand(0..@@colors_array.length - 1)])}
         end
         def choiceInput
             puts `clear`
+            p "Choices"
+            print @@colors_array
+            p ''
             p "Select color"
             tempC = gets.chomp
             isItAvailable = @@colors_array.any?{|color| color == tempC.downcase}
@@ -32,6 +38,13 @@ module  Mastermind
                 @@position_check_arr.push(@@wrong_position)
             end
         end
+        def winCondition
+            @@win = false
+            if (@@position_check_arr.all?{|mem| mem == 1})
+                @@win = true
+                p 'We have a winner'
+            end
+        end
         def compareInputCombination
             @@combination_arr.each_with_index do 
                 |comb_col, comb_index|
@@ -41,13 +54,30 @@ module  Mastermind
             @@position_check_arr = @@position_check_arr.sort
         end
         public 
-        def testMethod
-            newCombination
+        def playOneRound
             4.times{choiceInput}
             compareInputCombination
-            p @@combination_arr
-            p @@choice_arr
-            print @@position_check_arr
+            winCondition
+        end
+        def testMethod
+            newCombination
+            until @@win
+                playOneRound
+                tempArr = []
+                tempArr.push(@@choice_arr).push(@@position_check_arr)
+                @@round_arr.push(tempArr)
+                @@round_arr.each do
+                    |arr|
+                    p arr
+                end
+                p @@position_check_arr
+                unless @@win
+                    gets.chomp
+                    @@position_check_arr = []
+                    @@choice_arr = []
+                end
+            end
+            puts "Good job, you win"
         end
     end
 end
